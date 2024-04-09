57359ee8acdfb8b2be965be1dff995bddfaa7cb8
Shivang Kushwaha <shivangkushwaha025@gmail.com>
2024-04-09 22:59:09 +0530

    Removed the prod and test env for seeder and also removed the seeder for language

--- config/config.js
+++ config/config.js
@@ -12,23 +12,5 @@ module.exports = {
     dialect: process.env.DIALECT,
     seederStorage:"sequelize",
     seederStorageTableName: "sequelize_data"
-  },
-  test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.PASSWORD,
-    database: process.env.TEST_DB,
-    host: process.env.HOST, 
-    port: +process.env.PORT,
-    dialect: process.env.DIALECT,
-    seederStorageTableName: "sequelize_data"
-  },
-  production: {
-    username: process.env.PROD_DB_USERNAME, 
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
-    dialect: "mysql",
-    seederStorageTableName: "sequelize_data"
   }
 };
